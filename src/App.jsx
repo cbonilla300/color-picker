59b7de0620c3ab1a727c3bae86436e874b0d8aab
cesar bonilla <cbonilla300@hotmail.com>
2023-06-08 22:07:32 -0400

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const color = (prop) => {
-
+  // const [count, setCount] = useState(0)
+  const [selectedColor, setSelectedColor] = useState("")
+  const Color = (props) => {
+    return <div className={props.Color}>
+      <div onClick={() => setSelectedColor(Color)}></div>
+    </div>
+    
   }
 
   return (
     <>
-     <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
-    </div>
-      <div className={color}></div>
-      <div id="color-list">
-        <Color color="red" />
-        <Color color="blue"  />
-        <Color color="green" />
-      </div>
+    <div id="navbar">
+  <div>Currently selected: </div>
+  <div className={selectedColor}>{selectedColor}</div>
+</div>
+    { <div id="colors-list">
+  <Color Color="red" setSelectedColor={setSelectedColor} />
+  <Color Color="blue" setSelectedColor={setSelectedColor} />
+  <Color Color="green" setSelectedColor={setSelectedColor} />
+</div> }
+
+    
     </>
   )
 }
